b04a8ccd1d7e76e318f81d965fdc9b663e7041f2
Alfonso Rodríguez Ongay <alfonso.ro.96@gmail.com>
2025-08-25 20:31:28 -0600

    [add] files for whl

---
+++ movie_etl/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="movie_etl",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Your Name",
+    description="A simple ETL pipeline for movie ratings",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
